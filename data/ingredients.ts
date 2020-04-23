c780c9846293656ca7238aee5788ae19d7c87f80
kevinngth <kevin.ng.th@gmail.com>
2020-04-23 23:10:06 +0800

    added ingredients data

---
+++ data/ingredients.ts
@@ -0,0 +1,28 @@
+const ingredients = [
+    {
+        id: '1',
+        name: 'Chocolate 55%',
+        weight: 1000,
+        cost: 10,
+    },
+    {
+        id: '2',
+        name: 'Water',
+        weight: 1000,
+        cost: 0,
+    },
+    {
+        id: '3',
+        name: 'Sugar',
+        weight: 1500,
+        cost: 10,
+    },
+    {
+        id: '4',
+        name: 'Egg',
+        weight: 50,
+        cost: 10,
+    }
+];
+
+export default ingredients;
